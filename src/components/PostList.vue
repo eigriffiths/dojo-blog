6bbfe7baa25816b5a7fe977ea85eb137f291006f
Emyr Griffiths <Emyr.Griffiths@denbighshire.gov.uk>
2022-11-07 20:40:11 +0000

    dojo-blog

---
+++ src/components/PostList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+        <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+import SinglePost from './SinglePost.vue'
+
+export default {
+    name: 'PostList',
+    components: {SinglePost},
+    props: ['posts'],
+    setup(props) {
+        onMounted(() => {
+            console.log('mounted')
+        })
+        onUnmounted(() => {
+            console.log('unmounted')
+        })
+        onUpdated(() => {
+            console.log('compinent updated')
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
